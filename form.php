6e685a41ef52b22a5cd72e2a0e83439f286a8fe7
PinoGabriel <pinogabriel01@gmail.com>
2023-12-19 16:20:56 +0100

    feat: recupera la parola tante volte quanto richieste tramite form

--- form.php
+++ form.php
@@ -5,13 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css">
-    <title>Document</title>
+    <title>Form</title>
 </head>
 
 <body>
-    <form class="text-center m-5" action="password.php">
+    <form class="text-center m-5" action="password.php" method="GET">
         <label class="form-label" for="number">Lunghezza Password da generare: </label>
-        <input type="number" name="lunghezzaPassword" min="5" max="16">
+        <input class="ms-3" type="number" name="lunghezzaPassword" min="5" max="16">
+        <button class="btn btn-primary" type="submit">Genera</button>
     </form>
 </body>
 
--- password.php
+++ password.php
@@ -5,11 +5,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css">
-    <title>Document</title>
+    <title>PasswordGenerator</title>
 </head>
 
+<?php
+if (isset($_GET['lunghezzaPassword'])) {
+    $lunghezzaPassword  = $_GET['lunghezzaPassword'];
+}
+
+function generaPassword($lunghezza)
+{
+    $parola = "";
+    for ($i = 0; $i < $lunghezza; $i++) {
+        $parola .= "ciao";
+    }
+
+    return $parola;
+}
+
+$password = generaPassword($lunghezzaPassword)
+?>
+
 <body>
-    <?php ?>
+    <h1><?php echo $password; ?></h1>
 </body>
 
 </html>
