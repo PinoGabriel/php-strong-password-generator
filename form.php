3cbb17303b6112204eb09b3f3baca6445c1a6000
PinoGabriel <pinogabriel01@gmail.com>
2023-12-19 15:26:54 +0100

    first commit

---
+++ form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <form class="text-center m-5" action="password.php">
+        <label class="form-label" for="number">Lunghezza Password da generare: </label>
+        <input type="number" name="lunghezzaPassword" min="5" max="16">
+    </form>
+</body>
+
+</html>
