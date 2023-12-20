d89d582010d619793ce1a68eb07840004c8d950f
PinoGabriel <pinogabriel01@gmail.com>
2023-12-20 17:22:45 +0100

    feat: create random password with checkbox choose

--- form.php
+++ form.php
@@ -10,8 +10,38 @@
 
 <body>
     <form class="text-center m-5" action="password.php" method="GET">
-        <label class="form-label" for="number">Lunghezza Password da generare: </label>
-        <input class="ms-3" type="number" name="lunghezzaPassword" min="5" max="16" required>
+        <div class="container row my-5 mx-auto col-12">
+            <label class="form-label col-6" for="number">Lunghezza Password da generare: </label>
+            <input class="col-2" type="number" name="lunghezzaPassword" min="5" max="16" required>
+        </div>
+        <div class="container row my-5 mx-auto col-12">
+            <div>
+                <label class="form-check-label" for="lettereMinuscole">
+                    Lettere minuscole
+                </label>
+                <input class="form-check-input" type="checkbox" name="lettereMin" value="Lm" id="lettereMinuscole" checked>
+            </div>
+            <div>
+                <label class="form-check-label" for="lettereMaiuscole">
+                    Lettere Maiuscole
+                </label>
+                <input class="form-check-input" type="checkbox" name="lettereMax" value="LM" id="lettereMaiuscole">
+            </div>
+            <div>
+                <label class="form-check-label" for="numeri">
+                    Numeri
+                </label>
+                <input class="form-check-input" type="checkbox" name="numeri" value="N" id="numeri">
+            </div>
+            <div>
+                <label class="form-check-label" for="simboli">
+                    Simboli
+                </label>
+                <input class="form-check-input" type="checkbox" name="simboli" value="S" id="simboli">
+            </div>
+        </div>
+
+
         <button class="btn btn-primary" type="submit">Genera</button>
     </form>
 </body>
--- functions.php
+++ functions.php
@@ -1,26 +1,37 @@
 <?php
 
+$caratteri = '';
+
 if (isset($_GET['lunghezzaPassword'])) {
     $lunghezzaPassword = $_GET['lunghezzaPassword'];
 }
 
-$caratteri = [
-    'abcdefghijklmnopqrstuvwxyz',
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-    '0123456789',
-    '!#&?<>'
-];
+if (isset($_GET['lettereMin'])) {
+    $caratteri .= 'abcdefghijklmnopqrstuvwxyz';
+}
+
+if (isset($_GET['lettereMax'])) {
+    $caratteri .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+}
+
+if (isset($_GET['numeri'])) {
+    $caratteri .= '0123456789';
+}
+
+if (isset($_GET['simboli'])) {
+    $caratteri .= '!#&?';
+}
 
 function generaPassword($lunghezza, $caratteri)
 {
     $parola = "";
+    $lunghezzaTotale = strlen($caratteri);
     for ($i = 0; $i < $lunghezza; $i++) {
-        $categoriaCasuale = rand(0, 3);
-        $last = strlen($caratteri[$categoriaCasuale]) - 1;
-        $parola .= $caratteri[$categoriaCasuale][rand(0, $last)];
+        $indiceRandom = rand(0, $lunghezzaTotale - 1);
+        $parola .= $caratteri[$indiceRandom];
     }
 
-    return htmlspecialchars($parola);
+    return $parola;
 };
 
 $password = generaPassword($lunghezzaPassword, $caratteri);
